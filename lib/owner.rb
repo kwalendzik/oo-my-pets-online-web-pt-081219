ac137258ec6c35d69a1b93101cb4f397eea1db7d
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-07 00:49:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{self.dogs} dog(s), and #{self.cats} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
   
 end
